dd89660514b6b976aa774729635509a5aea6f5a1
Nancy Zhu <153876468+nancyhyzhu@users.noreply.github.com>
2025-02-22 13:44:02 -0500

    profile (not done)

--- roomies/src/pages/Profile.js
+++ roomies/src/pages/Profile.js
@@ -1,9 +1,26 @@
-
+import './Profile.css';
 
 function Profile() {
-    return (<div>
-        <h1>Profile Page!</h1>
-    </div>);
+    return (
+    <>
+    <div className="about-me-page">
+        <div className="sidebar">
+            <div className="profile-pic"></div>
+            <h2>name</h2>
+            <p>pronouns</p>
+            <h3>university</h3>
+
+            <ul class="social-links">
+                <li>⭐ email</li>
+                <li>⭐ instagram</li>
+                <li>⭐ discord</li>
+            </ul>
+        </div>
+        <div className="about-me-box">
+            <p className="about-me">About Me</p>
+        </div>
+    </div>
+    </>);
 }
 
 export default Profile;
