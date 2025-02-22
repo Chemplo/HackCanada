2579828ff5c84df6e8318201f4ce15f21e4e0116
sandy3w <sandyw.9372@gmail.com>
2025-02-22 14:31:14 -0500

    db text complete

--- roomies/src/pages/Home.js
+++ roomies/src/pages/Home.js
@@ -3,11 +3,19 @@ import './Home.css';
 function Home() {
     return (
         <div className="container">
-            <div className="questionnaire_box"></div>
+            <div className="questionnaire_box">
+                <h1 className="dbquestionnaire">Questionnaire</h1>
+            </div>
             <div className="second_row">
-                <div className="profile_box"></div>
-                <div className="result_box"></div>
-                <div className="about_box"></div>
+                <div className="result_box">
+                    <h2 className="dbresult">Results</h2>
+                </div>
+                <div className="profile_box">
+                    <h2 className="dbprofile">My Profile</h2>
+                </div>
+                <div className="about_box">
+                    <h2 className="dbabout">About</h2>
+                </div>
             </div>
         </div>
     );
